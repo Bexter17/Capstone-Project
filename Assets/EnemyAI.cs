54a09142d50c586e856ad935a1a0f22e7925fddc
Husky Fusky <bendermaj@gmail.com>
2020-08-04 23:02:46 -0400

    Added patrolling

--- Assets/EnemyAI.cs
+++ Assets/EnemyAI.cs
@@ -29,6 +29,7 @@ public class EnemyAI : MonoBehaviour
         {
             attackRange = 2;
         }
+        Patrol();
     }
 
     // Update is called once per frame
@@ -36,18 +37,26 @@ public class EnemyAI : MonoBehaviour
     {
         // Checks if the distance between enemy and player
         // is less then chaseRange
-        if (Vector3.Distance(target.position, gameObject.transform.position) < chaseRange && Vector3.Distance(target.position, gameObject.transform.position) > attackRange)
-        {
-            Chase();
+        //if (Vector3.Distance(target.position, gameObject.transform.position) < chaseRange && Vector3.Distance(target.position, gameObject.transform.position) > attackRange)
+        //{
+        //    Chase();
+        //}
 
-        }
+    }
+    private void MoveForward()
+    {
+        transform.position += transform.forward * enemyMovement * Time.deltaTime;
+    }
+    private void MoveContinuouslyForward()
+    {
+        rigidbody3D.velocity = new Vector2(playerDirection * transform.localScale.x * moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
     }
     private void Chase()
     {
         // Rotates to always face the player
-        transform.LookAt(target);
+        transform.LookAt(target, Vector3.up);
         // Move forward
-        transform.position += transform.forward * enemyMovement * Time.deltaTime;
+        MoveForward();
     }
     //private void Patrol()
     //{
@@ -61,4 +70,20 @@ public class EnemyAI : MonoBehaviour
     {
 
     }
+    private void Patrol()
+    {
+        transform.Rotate(0, 180, 0);
+        MoveForward();
+        Invoke("PatrolSwitch", 3);
+    }
+    private void PatrolSwitch()
+    {
+        transform.Rotate(0, 180, 0);
+        MoveForward();
+        Invoke("Patrol", 3);
+    }
 }
+
+        //Go in circles
+        //transform.Rotate(0, 1, 0);
+        //transform.position += transform.forward* enemyMovement * Time.deltaTime;
