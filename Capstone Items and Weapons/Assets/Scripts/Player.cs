5d95c6e194a20417a4fc0f25b94935c6457e376f
Bexter17 <63883117+Bexter17@users.noreply.github.com>
2020-08-10 15:26:20 -0400

    Basic Items
    Basic items and effects to player

---
+++ Capstone Items and Weapons/Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float health;
+    public float speed;
+    public float armor;
+    public float damage;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    //each power up has its own tag to do different things.
+     void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Health")
+        {
+            health++;
+            Debug.Log("health increased");
+        }
+        if(collision.gameObject.tag == "Armor")
+        {
+            armor++;
+            Debug.Log("Armor increased");
+        }
+        if (collision.gameObject.tag == "Speed")
+        {
+            armor++;
+            Debug.Log("Speed increased");
+        }
+        if (collision.gameObject.tag == "Damage")
+        {
+            armor++;
+            Debug.Log("Damage increased");
+        }
+    }
+
+
+}
