ba45ea2dd89c322feef30d4076724679105b6b82
Bexter17 <63883117+Bexter17@users.noreply.github.com>
2020-08-23 15:07:49 -0400

    MainHubScene

---
+++ Prototype/Assets/Scripts/Powerup.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup : MonoBehaviour
+{
+    
+    // activating power up for player
+    void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.CompareTag("Player"))
+        {
+            
+            Pickup();
+        }
+
+    }
+
+
+    void Pickup()
+    {
+        
+        Debug.Log("Power up picked up");
+        Destroy(gameObject);
+
+    }
+
+
+
+}
